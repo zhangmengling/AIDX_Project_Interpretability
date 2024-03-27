4bcac56d20277ed246e02f870e90f4ce1e21dd43
zhangmengling <zhangmengdi1997@126.com>
2024-03-28 00:14:58 +0800

    update

--- aisg_demo/backend/configs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class pConfig:
-    aPath = os.path.abspath('.')
-    path = aPath[:aPath.index('project')] + 'project/'
-    zip_path = path + "sav_demo/main/static/compressed/"
-    data_path = path + "sav_demo/main/static/datasets/"
-    model_path = path + "sav_demo/main/static/models/"
-    explanation_path = path + "sav_demo/main/static/explanation/"
